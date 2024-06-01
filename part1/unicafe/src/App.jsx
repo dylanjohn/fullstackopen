11221ad8ff3584dc40cd0e36459e6f94209f6194
dylanjohn <dylan@mediamachine.co.za>
2024-06-01 17:16:24 +0200

    Event handling revisited

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,80 +1,70 @@
 import { useState } from 'react'
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Display = props => <div>{props.value}</div>
+
 const App = () => {
-  // const [ counter, setCounter ] = useState(0)
-  // console.log('rendering with counter value', counter)
-
-  // const increaseByOne = () => setCounter(counter + 1)
-  // const increaseByTwo = () => setCounter(counter + 2)
-  // const decreaseByOne = () => setCounter(counter - 1)
-  // const setToZero = () => setCounter(0)
-
-  // const Display = ({ counter }) => <div>{counter}</div>
-
-  // const Button = ({ onClick, text}) => <button onClick={onClick}>{text}</button>
-
-  // const [clicks, setClicks] = useState({
-  //   left: 0, right: 0
-  // })
-
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-
   const [allClicks, setAll] = useState([])
   const [total, setTotal] = useState(0)
 
-  const handleLeftClick = () => {
-    // const newClicks = { 
-    //   ...clicks, 
-    //   left: clicks.left + 1 
-    // }
+  const [value, setValue] = useState(10)
 
+  const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
     const updatedLeft = left + 1
     setLeft(updatedLeft)
     setTotal(updatedLeft + right)
-
   }
 
   const handleRightClick = () => {
-    // const newClicks = { 
-    //   ...clicks, 
-    //   right: clicks.right + 1 
-    // }
- 
     setAll(allClicks.concat('R'))
     const updatedRight = right + 1
     setRight(updatedRight)
     setTotal(left + updatedRight)
   }
 
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+
   return (
     <>
-      {/* <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={increaseByTwo}
-        text='plus 2'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      /> */}
-
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
       <p>total {total}</p>
+    
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </>
+
   )
 }
 
