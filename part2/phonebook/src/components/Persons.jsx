f2c6e2963194695114f7b11289b820121854fead
dylanjohn <dylan@mediamachine.co.za>
2024-08-10 12:56:10 +0200

    Add proxy, check for empty list

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,6 +1,10 @@
 import Person from './Person'
 
 const Persons = ({ filteredPersons, handleRemove }) => {
+  if (filteredPersons.length === 0) {
+    return <p>No matching numbers found.</p>
+  }
+  
   return(
     <ul>
         {filteredPersons.map(person =>
